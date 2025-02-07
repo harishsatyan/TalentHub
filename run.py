de12f7db9a7890df1a9cf7cc8eeca89ddf5127a7
harishsatyan <harish.satyan@gmail.com>
2025-02-07 13:37:31 +0530

    Update run.py

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from waitress import serve
 if __name__ == '__main__':
     # serve(app,host='0.0.0.0',port=50100,threads=2)
     #app.run(host='10.120.56.105',debug=True)
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0',port=8000,debug=True)
